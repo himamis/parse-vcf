30766a988f5b97114f4f43bf614ba0c9fa247491
himamis <megette_a@yahoo.com>
2017-04-14 14:15:36 +0200

    Add filter field

---
+++ FilterField.hpp
@@ -0,0 +1,20 @@
+/*
+ * FilterField.hpp
+ *
+ *  Created on: 14 Apr 2017
+ *      Author: balazs
+ */
+
+#ifndef FILTERFIELD_HPP_
+#define FILTERFIELD_HPP_
+
+#include <string>
+
+namespace parsevcf {
+
+const std::string& id;
+const std::string& description;
+
+} // namespace parsevcf
+
+#endif /* FILTERFIELD_HPP_ */
