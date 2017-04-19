b67241ca66539cfe0e97304c84f3c91d6b99cdd1
himamis <megette_a@yahoo.com>
2017-04-19 15:22:12 +0200

    create test

---
+++ test/test-simple.cpp
@@ -0,0 +1,20 @@
+/* This file is part of the parsevcf library (GPL v2 or later), see LICENSE */
+#include "catch.hpp"
+
+#include "MetaInformation.h"
+#include "VCFParser.h"
+#include "DefaultHandler.h"
+#include <iostream>
+
+using namespace parsevcf;
+using namespace std;
+
+TEST_CASE("load simple file and parse") {
+	ifstream input;
+	input.open("examples/test-simple.vcf", ifstream::in);
+	DefaultHandler handler;
+
+	VCFParser parser = VCFParser(input, handler);
+
+	parser.parse();
+}
